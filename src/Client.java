ec1d9c45a3b52dc6013e55d6b11e1b87e14b8b2c
BrijPatel15 <brij.15@live.ca>
2019-01-14 09:11:12 -0500

    Client algorithm completed

--- src/Client.java
+++ src/Client.java
@@ -3,20 +3,25 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 
 public class Client {
 
 	private final String MODE_STRING = "ocTET";
 	private final String FILE_NAME = "Client.txt";
+	private static Logger clientLog = Logger.getLogger(Client.class.getName());
 	
     public static void main(String[] args) {
+    	clientLog.setLevel(Level.ALL);
         new Client().run();
     }
     
     public void run() {
     	try {
-			DatagramSocket socket = new DatagramSocket(1050);
+			DatagramSocket socket = new DatagramSocket();
 			for(int i = 0; i <= 10; i++) {
 				DatagramPacket sendPacket;
 				if(i % 2 == 0) {
@@ -25,16 +30,16 @@ public class Client {
 					sendPacket = createPacket(true, FILE_NAME);
 				}
 				String sentString = new String(sendPacket.getData());
-				System.out.println("Client: "+ sentString);
+				clientLog.log(Level.INFO,"Client: "+ sentString);
 				socket.send(sendPacket);
 				DatagramPacket recievePacket = new DatagramPacket(new byte[sendPacket.getData().length], sendPacket.getData().length);
 				socket.receive(recievePacket);
 				String recieveString = new String(recievePacket.getData());
-				System.out.println("Client: "+ recieveString);
+				clientLog.log(Level.INFO, "Client: "+ recieveString);
 			}
 			socket.close();
 		} catch (IOException e) {
-			System.out.print(e);
+			clientLog.log(Level.SEVERE, e.getMessage());
 		}
     	
     }
@@ -51,11 +56,12 @@ public class Client {
 		byte[] fileArray = new byte[initialArray.length + fileName.length()];
 		System.arraycopy(initialArray, 0, fileArray, 0, initialArray.length);
 		System.arraycopy(fileName.getBytes(), 0, fileArray, initialArray.length, fileName.getBytes().length);
-		
+		clientLog.log(Level.CONFIG, new String(fileArray));
 		byte[] finalArray = new byte[fileArray.length + MODE_STRING.length() + "0".length()];
 		System.arraycopy(fileArray, 0, finalArray, 0, fileArray.length);
 		System.arraycopy(MODE_STRING.getBytes(), 0, finalArray, fileArray.length, MODE_STRING.getBytes().length);
 		System.arraycopy("0".getBytes(), 0, finalArray, fileArray.length+MODE_STRING.getBytes().length, "0".getBytes().length);
+		clientLog.log(Level.CONFIG, new String(finalArray));
 		return new DatagramPacket(finalArray, finalArray.length, InetAddress.getLocalHost(), 23);
 	}
 	
--- src/Host.java
+++ src/Host.java
@@ -1,22 +1,26 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Host {
 
+	private static Logger hostLogger = Logger.getLogger(Host.class.getName());
+	
     public static void main(String[] args) {
         new Host().run();
     }
 	
     private void run() {
     	try {
-    		DatagramSocket socket = new DatagramSocket(23);
+    		DatagramSocket recieveSocket = new DatagramSocket(23);
     		while(true)
     		{
     			DatagramPacket recievePacket = new DatagramPacket(new byte[265], 265);
     		}
     	} catch(IOException e) {
-    		
+    		hostLogger.log(Level.SEVERE, e.getMessage());
     	}
     }
 }
